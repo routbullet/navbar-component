9757dc4926a10370670a59176128013489e94c00
Bullet Rout <bulletrout@Bullets-MacBook-Air.local>
2022-10-05 21:36:36 +0530

    navbar v1.0

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,32 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import "./sass/navbar.scss";
-import { Home, Account, Setting, Category } from "./icons";
-// import home from "/public/assets/home.svg";
+import { ICONS } from "./iconRoutes.js";
 
 export const NavbarComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <motion.div className="side-nav-container" animate={{ width: "290px" }}>
+    <motion.div className="side-nav-container" animate={{ width: "210px" }}>
       <div className="nav-logo">
-        <h2>Nav Logo</h2>
+        <h2>LOGO</h2>
+        <img src={ICONS.TOGGLE} style={{ width: "30px" }} alt="" />
       </div>
       <div className="nav-links">
         <ul>
           <li>
-            {/* <img src="../../public/assets/home.svg" alt="" /> */}
-            <Home logoWidth={30} />
+            <img src={ICONS.HOME} style={{ width: "30px" }} alt="" />
             <h3>Home</h3>
           </li>
           <li>
-            <Category logoWidth={30} />
+            <img src={ICONS.CATEGORY} style={{ width: "30px" }} alt="" />
+
             <h3>Category</h3>
           </li>
           <li>
-            <Setting logoWidth={30} />
+            <img src={ICONS.SETTING} style={{ width: "30px" }} alt="" />
+
             <h3>Setting</h3>
           </li>
           <li>
-            <Account logoWidth={30} />
+            <img src={ICONS.ACCOUNT} style={{ width: "30px" }} alt="" />
             <h3>Account</h3>
           </li>
         </ul>
